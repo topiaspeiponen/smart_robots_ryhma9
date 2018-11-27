c793d0263accad56ee74403f70bea9c4a66cc3e4
topiaspeiponen <43564489+topiaspeiponen@users.noreply.github.com>
2018-11-27 10:02:40 +0200

    Create zumo_config.h

---
+++ zumo_config.h
@@ -0,0 +1,16 @@
+#ifndef ZUMO_CONFIG_H_
+#define ZUMO_CONFIG_H_
+
+/* Change the following definition to 0 to disable MQTT and to 1 to enable it  */
+#define START_MQTT 1    
+
+/* The following definitions must all be in double quotes */
+#define NETWORK_SSID "V115 R9"
+
+#define NETWORK_PASSWORD "neinnein99"
+
+#define MQTT_BROKER "192.168.1.101"  
+    
+#define MQTT_CLIENT_ID "Zumo042"    
+
+#endif
