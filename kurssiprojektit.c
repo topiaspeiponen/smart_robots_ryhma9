0ea3ef7c3db11caaa5dfc645c9d9bcf4fe32f655
topiaspeiponen <43564489+topiaspeiponen@users.noreply.github.com>
2018-11-28 16:11:44 +0200

    Update kurssiprojektit.c

--- kurssiprojektit.c
+++ kurssiprojektit.c
@@ -1,5 +1,5 @@
-#if 0
-// Sumo-battle
+/*#if 0
+// Sumo-battle (alku)
 void zmain(void)
 {
     motor_start();
@@ -38,6 +38,75 @@ void zmain(void)
             break;
         }
     }
+}
+#endif
+*/
+
+#if 1
+// Sumo-battle (emilin version)
+void zmain(void)
+{
+motor_start();
+motor_forward(0,0);
+
+
+IR_Start();
+
+
+
+
+struct sensors_ dig;
+struct sensors_ ref;
+
+reflectance_start();
+reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
+
+while (SW1_Read() == 1) { //Odotetaan napinpainallusta koodin aloitukseen
+if (SW1_Read() == 0) {
+break;
+}
+vTaskDelay(100); 
+}
+
+for (;;) { //Aloitus looppi, mennään aloitusviivalle ja odotetaan IR-signaalia aloitukseen
+motor_forward(40, 150);
+
+reflectance_read(&ref);
+reflectance_digital(&dig);
+
+if (dig.r1 == 1 && dig.r2==1 && dig.r3==1 && dig.l1==1 && dig.l2==1 && dig.l3==1) {
+motor_forward(0,0);
+print_mqtt("Zumo042/ready", "line");
+IR_flush();
+IR_wait();
+motor_forward(100, 250);
+
+break;
+}
+}
+for(;;)
+{//sisällä ringissä peruuttaa viivalle tultaessa//
+motor_forward(255, 1);
+
+reflectance_read(&ref);
+reflectance_digital(&dig);
+
+if ( dig.l3==1) {
+motor_forward(0,0);
+print_mqtt("Zumo042/ready", "line");
+motor_backward(255, 100);
+motor_turn(125,0,1);;
+motor_forward(0,0); 
+}
+else if (dig.r3==1){
+motor_forward(0,0);
+print_mqtt("Zumo042/ready", "line");
+motor_backward(255, 100);
+motor_turn(0,125,1000);;
+motor_forward(0,0); 
+}
+}
+
 }
 #endif
 
